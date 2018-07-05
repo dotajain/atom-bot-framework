7206ff1e5174b197a0515d7e9f790d6f7d0237dd
Abhishek Jain <jainvabhi@gmail.com>
2018-07-05 16:12:59 +0530

    video updated

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ class App extends Component {
             
             <Logo />
             <h1>ATOM Insura</h1>
-            <h2>Your Insurance Assitent</h2>
+            <h2>Your Insurance Assistant</h2>
             <div className="text-left">
               <p>
                 How <strong>Artificial Intelligence</strong> is transforming the <br/>
@@ -37,6 +37,32 @@ class App extends Component {
           </div>
           <div>
             <ThemeProvider theme={theme}>
+              {
+                // <ChatBot
+                //   width="360px"
+                //   hideHeader
+                //   recognitionEnable              
+                //   steps={[
+                //     {
+                //       id: '1',
+                //       message: 'Hello Brian!, I am Insura, How can I help you today?',
+                //       trigger: 'search',
+                //     },
+                //     {
+                //       id: 'search',
+                //       user: true,
+                //       trigger: '3',
+                //     },
+                //     {
+                //       id: '3',
+                //       component: <RetailBotComponent />,
+                //       asMessage: true,
+                //       waitAction: true,
+                //       trigger: 'search',
+                //     },
+                //   ]}
+                // />
+            }
               <ChatBot
                 width="360px"
                 hideHeader
@@ -45,19 +71,81 @@ class App extends Component {
                   {
                     id: '1',
                     message: 'Hello Brian!, I am Insura, How can I help you today?',
-                    trigger: 'search',
+                    trigger: '2',
                   },
                   {
-                    id: 'search',
+                    id: '2',
                     user: true,
                     trigger: '3',
                   },
                   {
                     id: '3',
-                    component: <RetailBotComponent />,
-                    asMessage: true,
-                    waitAction: true,
-                    trigger: 'search',
+                    message: 'Sure. I can help you with that. I see that you have 2 auto policies linked to your account. For which policy do you need the information.',
+                    trigger: '4',
+                  },
+                  {
+                    id: '4',
+                    options: [
+                      { value: '67632971804', label: '67632971804', trigger: '5' },
+                      { value: '86652473895', label: '86652473895', trigger: '5' },
+                    ],
+                  },
+                  {
+                    id: '5',
+                    message: 'Thank you. Let me check',
+                    trigger: '6',
+                  },
+                  {
+                    id: '6',
+                    user: true,
+                    trigger: '7',
+                  },
+                  {
+                    id: '7',
+                    message: 'Thanks for waiting, your policy number 67632971804 is enforced stage.',
+                    trigger: '8',
+                  },
+                  {
+                    id: '8',
+                    message: 'Is there anything else you would like to know about this policy?',
+                    trigger: '9',
+                  },
+                  {
+                    id: '9',
+                    user: true,
+                    trigger: '10',
+                  },
+                  {
+                    id: '10',
+                    message: 'Premium due amount for policy 67632971804 is $6724.12',
+                    trigger: '11',
+                  },
+                  {
+                    id: '11',
+                    user: true,
+                    trigger: '12',
+                  },
+                  {
+                    id: '12',
+                    message: 'Due date for policy 67632971804 is July 15, 2018',
+                    trigger: '13',
+                  },
+                  {
+                    id: '13',
+                    message: 'Is there anything else I can help you with?',
+                    trigger: '14',
+                  },
+                  {
+                    id: '14',
+                    options: [
+                      { value: 'yes', label: 'Yes', trigger: '15' },
+                      { value: 'no', label: 'No', trigger: '15' },
+                    ],
+                  },
+                  {
+                    id: '15',
+                    message: 'Thanks for contacting me, I will be glad to help you next time. Have a great day.',
+                    end: true,
                   },
                 ]}
               />
